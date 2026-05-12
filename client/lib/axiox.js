36b34f4a20feadcd55af087950aa8271756f5ed7
DevDilip28 <dilipasdeo028@gmail.com>
2026-05-13 01:10:46 +0530

    backend url update

--- client/lib/axiox.js
+++ client/lib/axiox.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:1337/api",
+  baseURL: `${process.env.NEXT_PUBLIC_STRAPI_URL}/api`,
 });
 
 export default axiosInstance;
